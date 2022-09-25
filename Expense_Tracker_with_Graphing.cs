ce52dce38c4c1ab97b0f42f4a411a2c4e28d2d17
Kooreis <kooreis.official@gmail.com>
2022-09-25 05:00:00 -0500

    This update adds the main program class with a list to store expenses and a main method. The main method contains a loop that continuously prompts the user to select an option: add an expense, view expenses, or exit the program.

--- Expense_Tracker_with_Graphing.cs
+++ Expense_Tracker_with_Graphing.cs
@@ -1,10 +1,30 @@
 ```CSharp
-using System;
-using System.Collections.Generic;
-
-public class Expense
+public class Program
 {
-    public string Description { get; set; }
-    public decimal Amount { get; set; }
+    static List<Expense> expenses = new List<Expense>();
+
+    public static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("1. Add Expense");
+            Console.WriteLine("2. View Expenses");
+            Console.WriteLine("3. Exit");
+            Console.Write("Select an option: ");
+            var option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    AddExpense();
+                    break;
+                case "2":
+                    ViewExpenses();
+                    break;
+                case "3":
+                    return;
+            }
+        }
+    }
 }
 ```
