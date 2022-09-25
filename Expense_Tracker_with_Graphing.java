20a912bb9774d911003a7ba7c3bf47c89dd41dc5
Kooreis <kooreis.official@gmail.com>
2022-09-25 05:00:00 -0500

    This update includes the creation of the Main class and the main method. The main method includes a while loop that continuously runs the application until the user chooses to exit. The user is given three options: add an expense, view expenses, or exit the application.

--- Expense_Tracker_with_Graphing.java
+++ Expense_Tracker_with_Graphing.java
@@ -1,13 +1,27 @@
 ```java
-import java.util.*;
+public class Main {
+    static List<Expense> expenses = new ArrayList<>();
 
-class Expense {
-    String name;
-    double amount;
-
-    public Expense(String name, double amount) {
-        this.name = name;
-        this.amount = amount;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("1. Add Expense");
+            System.out.println("2. View Expenses");
+            System.out.println("3. Exit");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 1:
+                    addExpense(scanner);
+                    break;
+                case 2:
+                    viewExpenses();
+                    break;
+                case 3:
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
     }
-}
 ```
